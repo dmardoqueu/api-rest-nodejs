1d16e86778f460f31e5ee6a158b7ee254d2b32e9
dmardoqueu <diogomardoqueu@gmail.com>
2025-08-04 18:49:00 -0300

    Criando estrutura e banco de dados

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import fastify from 'fastify';
+import { knex } from './database';
+import { env } from './env';
+
+const app = fastify();
+
+app.get('/hello', async () => {
+    const transaction = await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title: 'Transação de teste',
+        amount: 1000,
+    }).returning('*')
+
+    return transaction
+})
+
+app.listen({
+    port: env.PORT
+}).then(() => {
+    console.log('HTTP Server running!')
+})
